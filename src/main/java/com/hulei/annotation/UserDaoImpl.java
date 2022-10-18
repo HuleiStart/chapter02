8de41268482992f831ef78c1351104c3fc321356
Mr.Hu <mr.hulei@qq.com>
2022-10-18 11:54:43 +0800

    基于注解方式注入

--- src/main/java/com/hulei/annotation/UserDaoImpl.java
+++ src/main/java/com/hulei/annotation/UserDaoImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
  * @create 2022/10/18 10:23
  *
  */
-//使用注解@Repository将UserDaoImpl类标识为Spring中的Bean--------相当于配置文件中<bean id="userDao" class="com.hulei/annotation.UserDaoImpl"/>
+//使用注解@Repository将UserDaoImpl类标识为Spring中的Bean--------相当于配置文件中<bean id="userDao" class="com.hulei.annotation.UserDaoImpl"/>
 @Repository("userDao")
 public class UserDaoImpl implements UserDao{
     @Override
